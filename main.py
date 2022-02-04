722c4988b5d6dab9d27ed774a2c8afffb1ade7d9
Andy Piper <andypiper@users.noreply.github.com>
2022-02-04 21:03:10 +0000

    Improved ❤️
    Different clear functions

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # inspired from https://twitter.com/GeekMomProjects/status/1483695065629224960
+# see https://dev.to/andypiper/bringing-the-bling-with-micropython-hn1
+
+# this is a deliberately simple script to test the various board functions
 
 import esp32
 from machine import Pin
@@ -8,8 +11,9 @@ import time
 
 
 # setup the NeoPixels
-# esp32.RMT.bitstream_channel(None) # does not work with this
-esp32.RMT.bitstream_channel(0)  # 01Space board needs this
+
+# esp32.RMT.bitstream_channel(None)  # does *not* work with this board
+esp32.RMT.bitstream_channel(0)       # board needs this for NeoPixels to work
 
 neopin = Pin(8, Pin.OUT)  # NeoPixel control on Pin 8
 pixels = 25  # we have 25 pixels, set as a constant here to use for loops
@@ -19,6 +23,7 @@ np = NeoPixel(neopin, pixels)
 # setup the button and the status LED
 button = Pin(9, Pin.IN)
 status_led = Pin(10, Pin.OUT)
+
 
 # scratchpad code testing
 
@@ -42,29 +47,68 @@ def rand_rgb():
     return r, g, b
 
 
-def test_all():
-    # Iterate through all Neopixels and switch them on and off
-    # with random colours in sequence
-    # Also output the chip internal temperature in centigrade
-    for x in range(pixels):
-        np[x] = (rand_rgb())
-        np.write()
-        print("Test pixel: " + str(x))
-        time.sleep(0.3)
-        np[x] = (0, 0, 0)
-        np.write()
-        time.sleep(0.3)
-    print("NeoPixels Test completed.")
-    # print("ESP32 temp measures: " + str((esp32.raw_temperature()-32)/1.8) + " degrees C")
-
-
-def clear():
+def clear_all():
     # Reset all Neopixels to black / off
+    # TODO: could have this use a custom colour
     np.fill((0, 0, 0))
     np.write()
 
 
+def clear_wipe():
+    # Reset all Neopixels to off with a wipe effect
+    lights = [0, 1, 2, 3, 4]
+    for x in lights:
+        np[x] = (0, 0, 0)
+        np.write()
+    time.sleep(0.2)
+    lights = [5, 6, 7, 8, 9]
+    for x in lights:
+        np[x] = (0, 0, 0)
+        np.write()    
+    time.sleep(0.2)
+    lights = [10, 11, 12, 13, 14]
+    for x in lights:
+        np[x] = (0, 0, 0)
+        np.write()
+    time.sleep(0.2)
+    lights = [15, 16, 17, 18, 19]
+    for x in lights:
+        np[x] = (0, 0, 0)
+        np.write()
+    time.sleep(0.2)
+    lights = [20, 21, 22, 23, 24]
+    for x in lights:
+        np[x] = (0, 0, 0)
+        np.write()
+    time.sleep(0.2)
+        
+
+def test_all_np():
+    # Iterate through all Neopixels and blink
+    # with random colours in sequence
+    clear_all()
+    for x in range(pixels):
+        np[x] = (rand_rgb())
+        np.write()
+        print("Pixel: " + str(x))
+        time.sleep(0.4)
+        np[x] = (0, 0, 0)
+        np.write()
+        time.sleep(0.2)
+    print("NeoPixels Test completed.")
+
+
+def test_led():
+    # Blink the status LED five times
+    for i in range(5):
+        status_led.on()
+        time.sleep(0.4)
+        status_led.off()
+        time.sleep(0.2)
+
+
 def smile():
+    clear_wipe()
     # Light the pixels into a smiley face
     lights = [1, 3, 10, 14, 16, 18, 22]
     for x in lights:
@@ -74,8 +118,9 @@ def smile():
 
 
 def heart():
+    clear_wipe()
     # Light the pixels into a heart shape
-    lights = [1, 3, 5, 6, 8, 9, 11, 12, 13, 17, 22]
+    lights = [1, 3, 5, 6, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 22]
     for x in lights:
         np[x] = (50, 0, 0)  # red heart (TODO: customisable colour)
     np.write()
@@ -83,5 +128,12 @@ def heart():
 
 
 # run the functions
-test_all()
+
+test_all_np()
+time.sleep(0.5)
+test_led()
+time.sleep(0.5)
 smile()
+time.sleep(1.0)
+heart()
+
